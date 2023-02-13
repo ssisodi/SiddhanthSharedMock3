e5b0fed72301b7742089af0bbb0c5116c6a6802b
Siddhanth <sisodiasiddhanth@gmail.com>
2023-02-13 20:53:54 +0530

    Assertions added to all the tests in TestNGClass1

--- LexusAutomationMaven/src/test/java/TestNg/TestNgClass1.java
+++ LexusAutomationMaven/src/test/java/TestNg/TestNgClass1.java
@@ -2,7 +2,8 @@ package TestNg;
 // Main TestNG Class // "G:\\My Drive\\HP Pavilion Backup\\Documents\\Notes-VCT\\chromedriver_win32\\chromedriver.exe");
 	import java.util.concurrent.TimeUnit;
 
-	import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -18,6 +19,7 @@ import LexusPomClasses.AccessoriesPage;
 	import LexusPomClasses.LexusHomePage;
 	import LexusPomClasses.PincodePage;
 	import LexusPomClasses.SummaryPage;
+import browser.Browser;
 
 import java.util.concurrent.TimeUnit;
 
@@ -44,7 +46,7 @@ import LexusPomClasses.LexusHomePage;
 import LexusPomClasses.PincodePage;
 import LexusPomClasses.SummaryPage;
 
-public class TestNgClass1 {
+public class TestNgClass1 extends Browser{
 	WebDriver driver;
 
 	@BeforeSuite
@@ -60,46 +62,30 @@ public class TestNgClass1 {
 	
 		if(browserName.equals("Chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver","G:\\My Drive\\HP Pavillion Backup\\Documents\\Notes-VCT\\chromedriver_win32_110\\chromedriver.exe");
-			driver = new ChromeDriver();
-			driver.get("https://www.lexus.com/");
-			driver.manage().window().maximize();
+			driver  = openChromeBrowser(); 
 		}
 		if(browserName.equals("Firefox"))
 		{
-			System.setProperty("webdriver.gecko.driver","G:\\My Drive\\HP Pavillion Backup\\Documents\\Notes-VCT\\geckodriver-v0.32.1-win32\\geckodriver.exe");
-			driver = new FirefoxDriver();
-			driver.get("https://www.lexus.com/");
-			driver.manage().window().maximize();
+			driver = openFireFoxBrowser(); 
 		}
 		if(browserName.equals("Opera"))
 		{
-			System.setProperty("webdriver.opera.driver","G:\\My Drive\\HP Pavillion Backup\\Documents\\Notes-VCT\\operadriver_win32\\operadriver_win32\\operadriver.exe");
-			driver = new OperaDriver();
-			driver.get("https://www.lexus.com/");
-			driver.manage().window().maximize();
+			driver = openOperaBrowser(); 
 		}
 		if(browserName.equals("MicrosoftE"))
 		{
-			System.setProperty("webdriver.edge.driver","G:\\My Drive\\HP Pavillion Backup\\Documents\\Notes-VCT\\edgedriver_win64\\msedgedriver.exe");
-			driver = new EdgeDriver();
-			driver.get("https://www.lexus.com/");
-			driver.manage().window().maximize();
+			driver = openMicrosoftEBrowser(); 
 		}
 		
 		System.out.println("Before test");
 	}
-/*
+
 	@BeforeClass
 	public void defaultDriver() {
-	System.setProperty("webdriver.chrome.driver","G:\\My Drive\\HP Pavillion Backup\\Documents\\Notes-VCT\\chromedriver_win32_110\\chromedriver.exe");
-	driver = new ChromeDriver();	
-	/*System.setProperty("webdriver.gecko.driver","C:\\Users\\SID9T\\OneDrive\\Documents\\Notes-VCT\\geckodriver.exe");
-	driver = new FirefoxDriver();
 	driver.get("https://www.lexus.com/");
 	driver.manage().window().maximize();	
 	}
-*/
+
 	@BeforeMethod
 	public void beforeMethod() throws InterruptedException
 	{	
@@ -130,7 +116,7 @@ public class TestNgClass1 {
 	 /* Hard assertion is replaced for 'if-else statement' 
 	  if(result.equals(expresult))
 	  {
-		  System.out.println("Vehicles dropdown menu successfull");
+		  System.out.println("Vehicles dropdown menu successful");
 		  homepage.cursorOnVehicleTab();
 	  }
 	  else 
@@ -139,7 +125,7 @@ public class TestNgClass1 {
 	  }
 	 */
 	  Thread.sleep(2000);
-	//-----------------------------------------------------------
+	//-------------------------------------------------------
 	  String exptext1 = "SEDAN123"; 
 	  String exptext2 = "HYBRID & ELECTRIC"; 
 	  String exptext3 = "PERFORMANCE123"; 
@@ -159,12 +145,12 @@ public class TestNgClass1 {
 	  /*
 	  if(result1.equals(exptext1) && result2.equals(exptext2) && result3.equals(exptext3) && result4.equals(exptext4)) 
 	  {
-		  System.out.println("Tabs validation successfull");
+		  System.out.println("Tabs validation successful");
 		  homepage.moveOverAllTabs();
 	  }
 	  else
 	  {
-		  System.out.println("Tabs validation unsuccessfull");
+		  System.out.println("Tabs validation unsuccessful");
 	  }*/
  	  Thread.sleep(2000); 
 	//-----------------------------------------------------------		
@@ -210,9 +196,15 @@ public class TestNgClass1 {
 	  }*/
 	  
 	  }
-	  catch(Exception e)
+	  catch(NoSuchElementException e)
 	  {
-		  System.out.println(e);
+		  LexusHomePage homepage = new LexusHomePage(driver);
+		  homepage.cursorOnVehicleTab();
+		  homepage.moveOverAllTabs();  
+	  }
+	  catch(Exception f)
+	  {
+		  System.out.println("Dropped to the last exception catch block");
 	  }
   }
   
@@ -220,7 +212,7 @@ public class TestNgClass1 {
   public void Test2() throws InterruptedException
   {
 	  try {
-		  System.out.println("-------------------------------------------------");
+	  System.out.println("-------------------------------------------------");
 	  System.out.println("Test case 2 - Pincode & Car selection page");
 	  PincodePage pincode = new PincodePage(driver);
 	  SoftAssert soft = new SoftAssert();
@@ -264,7 +256,7 @@ public class TestNgClass1 {
 	  Thread.sleep(2000);
 	  String acttext = pincode.verifyCarTitle(); 
 	  System.out.println("Actual car title: " + acttext);
-	  String exptext = "2023 UX 250h F SPORT DESIGN(wrong)";
+	  String exptext = "2023 UX 250h F SPORT DESIGN";
 	  soft.assertEquals(acttext, exptext, "Car title not matched");
 	  soft.assertAll();
 	  
@@ -281,16 +273,17 @@ public class TestNgClass1 {
 		  System.out.println("Car title not matched");
 	  }
 	  */
+	  Thread.sleep(2000);
 	  pincode.scrollToMyCar();// Car title searched by scrolling down 
-	  
+	  Thread.sleep(3000);
 	  }
 	  catch(Exception e)
 	  {
 		  System.out.println(e);
 	  }
-  }  
+ }  
   
-  @Test (enabled = false)
+  @Test 
 	public void Test3() throws InterruptedException
 {
 	  try {
@@ -301,6 +294,10 @@ public class TestNgClass1 {
 		String actcolor = exteriorcolor.verifyExtColor(); 
 		System.out.println(actcolor);
 		String expcolor = "Cloudburst Gray* with Obsidian roof";
+		
+		Assert.assertEquals(actcolor, expcolor,"Cloudburst Gray* with Obsidian roof color not selected");
+		System.out.println("Cloudburst Gray* with Obsidian roof color selected");
+		/*
 		if(actcolor.equals(expcolor))
 		{
 			System.out.println("Exterior color type is matched");
@@ -309,9 +306,13 @@ public class TestNgClass1 {
 		{
 			System.out.println("Exterior color type is not matched");
 		}
+		*/
 		//-----------------------------------------------------------
 		Thread.sleep(2000);
 		boolean nexttab = exteriorcolor.testNextButton(); 
+		Assert.assertTrue(nexttab, "Next:Interior button is disabled");
+		System.out.println("Next:Interior button is enabled");
+		/*
 		if(nexttab == true)
 		{
 			System.out.println("Next:Interior button is enabled");
@@ -319,8 +320,8 @@ public class TestNgClass1 {
 		else 
 		{
 			System.out.println("Next:Interior button is disabled");
-		}		
-		
+		}
+		*/
 		exteriorcolor.clickNext(); // Clicking the Next:Interior tab after testing
 	 }
 	  catch(Exception e)
@@ -329,17 +330,21 @@ public class TestNgClass1 {
 	  	}
 }
   
-	@Test (enabled = false)
+	@Test
 	public void Test4() throws InterruptedException
 	{
 		try {
-			System.out.println("-------------------------------------------------");	
-			System.out.println("Test case 4 - Interior color page");
+		System.out.println("-------------------------------------------------");	
+		System.out.println("Test case 4 - Interior color page");
 		InteriorColorPage interiorcolor = new InteriorColorPage(driver);
 		interiorcolor.selectIntColor(); // Interior color selected before testing 
 		String actIntcolor = interiorcolor.verifyIntColor(); 
 	//	System.out.println(actIntcolor);
 		String expIntcolor = "Black NuLuxe® and Black washi dash trim"; 
+		
+		Assert.assertEquals(actIntcolor, expIntcolor, "Interior color is not matched");
+		System.out.println("Interior color is matched");
+		/*
 		if(actIntcolor.equals(expIntcolor))
 		{
 			System.out.println("Interior color is matched");
@@ -348,10 +353,14 @@ public class TestNgClass1 {
 		{
 			System.out.println("Interior color is not matched");
 		}
-		
+		*/
 		//-----------------------------------------------------------
 		Thread.sleep(2000);
+		
 		boolean nextpac = interiorcolor.testnextPackage(); 
+		Assert.assertTrue(nextpac, "Next:Package button is disabled");
+		System.out.println("Next:Package button is enabled");
+		/*
 		if(nextpac == true)
 		{
 			System.out.println("Next:Package button is enabled");
@@ -360,8 +369,10 @@ public class TestNgClass1 {
 		{
 			System.out.println("Next:Package button is disabled");
 		}
-		interiorcolor.clickNext();// Next package button clicked after testing 
-		 System.out.println("-------------------------------------------------");
+		*/
+		interiorcolor.clickNext();// Next package button clicked after testing
+		
+		System.out.println("-------------------------------------------------");
 		}
 		catch(Exception e)
 		{
@@ -369,7 +380,7 @@ public class TestNgClass1 {
 		}
 	}
 	
-	@Test(enabled = false)
+	@Test
 	public void Test5()
 	{
 		try {
@@ -379,6 +390,10 @@ public class TestNgClass1 {
 			//AUTO-DIMMING REARVIEW MIRROR
 			String actmatch1 = accessories.verifyAutoDim(); 
 			String expmatch1 = "AUTO-DIMMING REARVIEW MIRROR";
+			Assert.assertEquals(actmatch1, expmatch1, "Cannot add this Item 1");
+			accessories.add1stItem();
+			System.out.println("Item 1 got added");
+			/*
 			if(actmatch1.equals(expmatch1))
 			{
 				accessories.add1stItem(); // AUTO-DIMMING REARVIEW MIRROR item is added
@@ -388,9 +403,15 @@ public class TestNgClass1 {
 			{
 				System.out.println("Cannot add this Item 1");
 			}
+			*/
+			
 			//COLOR HEAD-UP DISPLAY
 			String actmatch2 = accessories.verifyHeadUp(); 
 			String expmatch2 = "COLOR HEAD-UP DISPLAY";
+			Assert.assertEquals(actmatch2, expmatch2, "Cannot add this Item 2");
+			accessories.add2ndItem();
+			System.out.println("Item 2 got added");
+			/*			
 			if(actmatch2.equals(expmatch2))
 			{
 				accessories.add2ndItem();// COLOR HEAD-UP DISPLAY item is added
@@ -399,9 +420,15 @@ public class TestNgClass1 {
 			{
 				System.out.println("Cannot add this Item 2");
 			}
+			*/
+			
 			//WIRELESS CHARGER
 			String actmatch3 = accessories.verifyWirelessC(); 
 			String expmatch3 = "WIRELESS CHARGER*";
+			Assert.assertEquals(actmatch3, expmatch3, "Cannot add this Item 3");
+			accessories.add3rdItem();//WIRELESS CHARGER item is added
+			System.out.println("Item 3 got added");
+			/*
 			if(actmatch3.equals(expmatch3))
 			{
 				accessories.add3rdItem();//WIRELESS CHARGER item is added
@@ -411,9 +438,15 @@ public class TestNgClass1 {
 			{
 				System.out.println("Cannot add this Item 3");
 			}
+			*/
+			
 			//INTUITIVE PARKING ASSIST
 			String actmatch4 = accessories.verifyIntuitPark(); 
 			String expmatch4 = "INTUITIVE PARKING ASSIST*";
+			Assert.assertEquals(actmatch4, expmatch4, "Cannot add this Item 4");
+			accessories.add4thItem();
+			System.out.println("Item 4 got added");
+			/*
 			if(actmatch4.equals(expmatch4))
 			{
 				accessories.add4thItem();
@@ -422,10 +455,16 @@ public class TestNgClass1 {
 			else 
 			{
 				System.out.println("Cannot add this Item 4");
-			}
+			}*/
+			
+			
 			//POWER REAR DOOR WITH KICK SENSOR	
 			String actmatch5 = accessories.verifyPowerSensor(); 
 			String expmatch5 = "POWER REAR DOOR WITH KICK SENSOR"; 
+			Assert.assertEquals(actmatch5, expmatch5, "Cannot add this Item 5");
+			accessories.add5thItem();
+			System.out.println("Item 5 got added");
+			/*			
 			if(actmatch5.equals(expmatch5))
 			{
 				accessories.add5thItem();
@@ -435,9 +474,15 @@ public class TestNgClass1 {
 			{
 				System.out.println("Cannot add this Item 5");
 			}
+			*/
+			
 			//HEATED STEERING WHEEL
 			String actmatch6 = accessories.verifyHeaterWheel(); 
 			String expmatch6 = "HEATED STEERING WHEEL"; 
+			Assert.assertEquals(actmatch6, expmatch6, "Cannot add this Item 6 & 7");
+			accessories.add6thItem();
+			System.out.println("Item 6 & 7 got added");
+			/*
 			if(actmatch6.equals(expmatch6))
 			{
 				accessories.add6thItem();
@@ -446,10 +491,15 @@ public class TestNgClass1 {
 			else {
 				System.out.println("Cannot add this Item 6 & 7");
 			}
+			*/
 			Thread.sleep(3000);
 			//WINDSHIELD WIPER DE-ICER
 			String actmatch7 = accessories.verifyDeIcer(); 
 			String expmatch7 = "WINDSHIELD WIPER DE-ICER";
+			Assert.assertEquals(actmatch7, expmatch7, "Cannot add this Item 8");
+			accessories.add8Item();
+			System.out.println("Item 8 got added");
+			/*
 			if(actmatch7.equals(expmatch7))
 			{
 				accessories.add8Item();
@@ -459,8 +509,13 @@ public class TestNgClass1 {
 			{
 				System.out.println("Cannot add this Item 8");
 			}
+			*/
 			// Next: Accessories
 			boolean actmatch8 = accessories.verifyNextAcc(); 
+			Assert.assertTrue(actmatch8, "Next: Accessories is disabled");
+			System.out.println("Next: Accessories is enabled");
+			accessories.clickNextAcc();
+			/*
 			if(actmatch8 == true)
 			{
 				System.out.println("Next: Accessories is enabled");
@@ -469,7 +524,8 @@ public class TestNgClass1 {
 			else
 			{
 				System.out.println("Next: Accessories is disabled");
-			}			
+			}		
+			*/	
 		}
 		catch(Exception e)
 		{
@@ -477,7 +533,7 @@ public class TestNgClass1 {
 		}
 	}
 
-	@Test(enabled = false)
+	@Test
 	public void Test6() throws InterruptedException
 	{
 		try {
@@ -486,6 +542,11 @@ public class TestNgClass1 {
 		
 		SummaryPage summarypage = new SummaryPage(driver); 
 		boolean sumpage = summarypage.verifyNextSum();
+		Assert.assertTrue(sumpage, "Next: Summary tab is disabled");
+		System.out.println("Next: Summary tab is enabled");
+		Thread.sleep(3000);
+		summarypage.nextSum();
+		/*
 		if(sumpage == true)
 		{
 			System.out.println("Next: Summary tab is enabled");
@@ -495,11 +556,16 @@ public class TestNgClass1 {
 		else 
 		{
 			System.out.println("Next: Summary tab is disabled");
+		}*/
 		}
-			}
-		catch(Exception e)
+		catch(NoSuchElementException e)
 		{
-			System.out.println("Error is:  " + e);
+			SummaryPage summarypage = new SummaryPage(driver);
+			summarypage.nextSum();			
+		}
+		catch(Exception f)
+		{
+			System.out.println("Reached the last catch block for summary page with this exception" + f);
 		}
 	}
 	
